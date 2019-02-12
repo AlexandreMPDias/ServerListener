be110228511aa4d135c309fc9b8cba151017f085
Tijuk Squach <alexandre.dias@liberedu.com>
2019-02-12 09:57:56 -0200

    A very pretty commit

--- src/led_handler.js
+++ src/led_handler.js
@@ -1,46 +1,49 @@
-let gpio = require('onoff').Gpio;
-let LED = new gpio(3, 'out');
+let gpio = require("onoff").Gpio;
+let LED = new gpio(3, "out");
+let BUTTON = new gpio(2, "in", "both");
 
-let state = 0;
+button.watch((err, value) => {
+  console.log(err, value);
+  led.writeSync(0);
+});
 
 function initLED(pin) {
-	LED = new gpio(pin, 'out');
+  LED = new gpio(pin, "out");
 }
 
 function turnOn() {
-	LED.writeSync(1);
+  LED.writeSync(1);
 }
 
 function turnOff() {
-	LED.writeSync(0);
+  LED.writeSync(0);
 }
 
 function blink() {
-	if(LED.readSync == 0) {
-		turnOn();
-	} else {
-		turnOff();
-	}
+  if (LED.readSync == 0) {
+    turnOn();
+  } else {
+    turnOff();
+  }
 }
 
 let blinkInterval = null;
 
 function startBlinking() {
-	blinkInterval = setInterval(blink, 100);
+  blinkInterval = setInterval(blink, 100);
 }
 
 function stopBlinkingAfter(time) {
-	setTimeout( () => {
-		clearInterval(blinkInterval);
-		LED.writeSync(0);
-	}, time);
+  setTimeout(() => {
+    clearInterval(blinkInterval);
+    LED.writeSync(0);
+  }, time);
 }
 
 module.exports = {
-	turnOn,
-	turnOff,
-	initLED,
-	startBlinking,
-	stopBlinkingAfter
-	
-}
+  turnOn,
+  turnOff,
+  initLED,
+  startBlinking,
+  stopBlinkingAfter
+};
